8bf83e79d157572962427e5ad9ec0d3781afe1cb
sahilgaikar757-svg <sahilgaikar757@gmail.com>
2026-07-30 22:05:34 +0530

    Create Mergesort.java

---
+++ Mergesort.java
@@ -0,0 +1,68 @@
+public class MergeSortPhone {
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] digits = {7, 0, 2, 0, 9, 8, 7, 7, 3, 6};
+
+        System.out.print("Original Digits: ");
+        for (int num : digits)
+            System.out.print(num + " ");
+
+        mergeSort(digits, 0, digits.length - 1);
+
+        System.out.print("\nSorted Digits: ");
+        for (int num : digits)
+            System.out.print(num + " ");
+    }
+}
